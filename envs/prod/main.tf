c8cd02d6681020bba6073435f20c76a660181fb9
Pratham Sharma <124365058+pratham7289@users.noreply.github.com>
2026-03-08 19:36:35 +0545

    Refactor Terraform configuration for production setup
    Updated the VPC, EC2, and S3 module configurations for production environment with new CIDR block, availability zones, and instance types.

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,35 +1,23 @@
-
-// The local block will create a variable called name_prefix that concatenates the project and environment variables. 
-//This can be used to create unique resource names across different environments.
 locals {
   name_prefix = "${var.project}-${var.env}"
 }
 
-
-// The vpc module will create a VPC with the specified CIDR block and availability zones.
-// The values are passed here using the local.name_prefix variable to ensure that the VPC name is unique across different environments.
-
 module "vpc" {
-  source      = "D:\\TerraformProjects\\Advanced-Lab\\Multi-Env\\modules\\vpc"
-  name_prefix = "${local.name_prefix}-vpc"
-  cidr_block  = "10.50.0.0/16"
-  az_count    = 2
+  source   = "../../modules/vpc"
+  name     = "${local.name_prefix}-vpc"
+  cidr     = "10.50.100.0/16"  # Production CIDR
+  az_count = 3                  # More AZs for prod
 }
 
-
-// The below is the ec2 module called
-// The ec2 module will create an EC2 instance with the specified instance type, AMI ID, and subnet ID.
-// The values are passed here using the local.name_prefix variable to ensure that the EC2 instance
 module "ec2" {
-  source        = "D:\\TerraformProjects\\Advanced-Lab\\Multi-Env\\modules\\ec2"
-  name_prefix   = "${local.name_prefix}-ec2"
-  instance_type = "t3.micro"
-  ami_id        = "ami-0c94855ba95c71c99" // Amazon Linux 2 AMI (HVM), SSD Volume Type
+  source        = "../../modules/ec2"
+  name          = "${local.name_prefix}-toolbox"
+  ami           = "ami-08c40ec9ead489470" # Update to prod AMI if needed
+  instance_type = "t3.medium"            # Bigger instance for prod
   subnet_id     = module.vpc.public_subnet_ids[0]
 }
 
-
 module "s3" {
-  source      = "D:\\TerraformProjects\\Advanced-Lab\\Multi-Env\\modules\\s3"
-  bucket_name = "${local.name_prefix}-bucket"
+  source      = "../../modules/s3"
+  bucket_name = "${local.name_prefix}-bucket-12345-prod"
 }
